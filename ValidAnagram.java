40582dd064e4c7363314e7c29f8ae2e484d80632
github-sid <emailsiddharth1@gmail.com>
2026-02-11 15:44:53 -0500

    Add ValidAnagram solutions

--- ValidAnagram.java
+++ ValidAnagram.java
@@ -18,7 +18,24 @@ class Solution {
             {
                 return false;
             }
+        
+        
         } 
+        //  char[] ss = s.toCharArray();
+
+        // Arrays.sort(ss);
+
+        // char[] tt = t.toCharArray();
+
+        // Arrays.sort(tt);
+        // for(int i =0; i<ss.length;i++)
+        // {
+        //     if(ss[i] != tt[i])
+        //     {
+        //         return false;
+        //     }
+        // }
+        
         return true;             
     }
 }
